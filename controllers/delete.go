c55afb1b2a363d4d1125d543cde2639adb0fb2d7
Benji Hooper <benji@sixp.in>
2018-12-18 12:29:27 +0000

    Fixed a bug where delete could not handle deleting duplicates

--- controllers/delete.go
+++ controllers/delete.go
@@ -1,44 +1,45 @@
 package controllers
 
 import (
-	"fmt"
-
 	"github.com/benjih/bookmark-service/dao"
 	"github.com/benjih/bookmark-service/model"
 )
 
 func DeleteBookmarkByUrls(urls []string) error {
-	fmt.Println(urls)
-
 	bookmarks, err := dao.RetrieveBookmarks()
 	if err != nil {
 		return err
 	}
 
-	deleteBookmarkFromTreeByURLs(bookmarks, urls)
+	bookmarks = deleteBookmarkFromTreeByURLs(bookmarks, urls)
 
-	fmt.Println(bookmarks)
-
-	dao.StoreBookmarks(bookmarks)
-	return nil
+	return dao.StoreBookmarks(bookmarks)
 }
 
-func deleteBookmarkFromTreeByURLs(bookmarks []*model.Bookmark, urls []string) {
+func deleteBookmarkFromTreeByURLs(bookmarks []*model.Bookmark, urls []string) []*model.Bookmark {
 	bookmarksToDelete := []int{}
 	for i, bookmark := range bookmarks {
 		for _, url := range urls {
 			if url == bookmark.URL {
+				// TODO: This will delete a parent and all its entries
+
+				// Define what behaviour should happen in this scenario
 				bookmarksToDelete = append(bookmarksToDelete, i)
 				continue
 			}
 
 			if bookmark.Entries != nil {
-				deleteBookmarkFromTreeByURLs(bookmark.Entries, urls)
+				bookmark.Entries = deleteBookmarkFromTreeByURLs(bookmark.Entries, urls)
 			}
 		}
 	}
 
+	var positionsDeleted = 0
 	for _, positionToDelete := range bookmarksToDelete {
-		bookmarks = append(bookmarks[:positionToDelete], bookmarks[positionToDelete+1:]...)
+		adjustedPositionToDelete := positionToDelete - positionsDeleted
+		bookmarks = append(bookmarks[:adjustedPositionToDelete], bookmarks[adjustedPositionToDelete+1:]...)
+		positionsDeleted = positionsDeleted + 1
 	}
+
+	return bookmarks
 }
