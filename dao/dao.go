e51516b93d76d18c2b8fe97e14a83016cb4b80a1
Benji Hooper <benji@sixp.in>
2018-12-18 11:34:30 +0000

    Adding GlobalDAO to be used in the controllers and setting the default DAO to InMemory for the time being

--- dao/dao.go
+++ dao/dao.go
@@ -1,8 +1,20 @@
 package dao
 
-import "github.com/benjih/bookmark-service/model"
+import (
+	"github.com/benjih/bookmark-service/dao/mem"
+	"github.com/benjih/bookmark-service/model"
+)
 
 type DAO interface {
-	StoreBookmarks([]*model.Bookmark) error
+	StoreBookmarks(bookmarks []*model.Bookmark) error
 	RetrieveBookmarks() ([]*model.Bookmark, error)
+}
+
+var GlobalDAO = mem.NewInMemoryDAO()
+
+func StoreBookmarks(bookmarks []*model.Bookmark) error {
+	return GlobalDAO.StoreBookmarks(bookmarks)
+}
+func RetrieveBookmarks() ([]*model.Bookmark, error) {
+	return GlobalDAO.RetrieveBookmarks()
 }
